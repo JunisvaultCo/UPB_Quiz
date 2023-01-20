aed761c161488d994302e8531963b9111505c72b
Plăiașu Iulia-Silvia <e.email72@yahoo.ro>
2023-01-20 16:42:34 +0200

    adding main page

--- index.js
+++ index.js
@@ -6,8 +6,29 @@ xhr.onreadystatechange = function()
 {
     if (xhr.readyState === 4)
     {
-        var tests = JSON.parse(xhr.responseText);
+        tests = JSON.parse(xhr.responseText).list;
     }
 };
 xhr.send();
 console.log(tests);
+
+let content = document.getElementById("body");
+let btn = document.createElement('input');
+
+btn.type= 'button';
+btn.id='all';
+btn.value='Run All';
+
+btn.addEventListener('click', function(e)
+{
+});
+content.insertBefore(btn,content.childNodes[0]);
+let lastBtn = btn;
+for (let i = 0; i < tests.length; i++) {
+    
+    btn = document.createElement('input');
+    btn.type= 'button';
+    btn.id=tests[i].name;
+    btn.value='Run ' + btn.id;
+    content.insertAfter(btn, lastBtn);
+}
